78e145a2a90eb123f95c20f1f3588052378c6d0d
Benjamin Devaublanc <bdevaublanc@molotov.tv>
2016-03-14 15:36:53 +0100

    init project

---
+++ app/index.jsx
@@ -0,0 +1,7 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './web_modules/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
